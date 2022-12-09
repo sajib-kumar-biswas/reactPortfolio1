529b3a3accd98811f60dd6ce09cef0675cc10e13
sajib_kumar_biswas <sajibbiswas01521457841@gmail.com>
2022-12-09 09:49:12 +0600

    dont till experience section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,52 @@
 import './about.css';
+import ME from '../../images/sajib.jpg'
+
+import {FaAward} from 'react-icons/fa';
+import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
+
+
 
 const About = () => {
     return (
-        <div className="">
-            <h1>About</h1>
-        </div>
+        <section id="about">
+            <h5>Get to know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="about me" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <FaAward className='about__icon' />
+                            <h5>Experience</h5>
+                            <small>1+ Years Working</small>
+                        </article>
+
+                        <article className="about__card">
+                            <FiUsers className='about__icon' />
+                            <h5>Clients</h5>
+                            <small>1+ Worldwide</small>
+                        </article>
+
+                        <article className="about__card">
+                            <VscFolderLibrary className='about__icon' />
+                            <h5>Projects</h5>
+                            <small>10+ Completed</small>
+                        </article>
+                    </div>
+
+                    <p>I am solving problems on different coding platforms like codeforces, codechef, atcoder, geeksforgeeks, leetcode since second year of my university life. Now I am learning fullstack development using MERN stack technology. I usually read documentation and try to do some small project using things i learnt. I am really passionate to pursue my career in web development.</p>
+
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,21 @@
 import './nav.css';
 
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai';
+import {BiBookAlt, BiMessageSquareDetail} from 'react-icons/bi';
+import {RiServiceLine} from 'react-icons/ri'
+import { useState } from 'react';
+
 const Nav = () => {
-    
+    const [activeNav, setActiveNav] = useState("#home");
+
     return (
-        <div className="">
-            <h1>Nav</h1>
-        </div>
+        <nav>
+            <a href="#home" onClick={() => setActiveNav("#home")} className={activeNav==='#home' ? "active": ""}><AiOutlineHome/></a>
+            <a onClick={(e) => setActiveNav("#about")} className={activeNav==="#about" ? "active": ""} href="#about"><AiOutlineUser /></a>
+            <a onClick={e => setActiveNav("#experience")} className={activeNav==="#experience" ? "active": ""} href="#experience"><BiBookAlt /></a>
+            <a onClick={e => setActiveNav("#services")} className={activeNav==="#services" ? "active": ""} href="#services"><RiServiceLine /></a>
+            <a onClick={e => setActiveNav("#contact")} className={activeNav==="#contact" ? "active": ""} href="#contact"><BiMessageSquareDetail /></a>
+        </nav>
     )
 }
 
